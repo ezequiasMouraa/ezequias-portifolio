784306866dc1b040a5c257b2baa9535af081c2d7
ezequiasMouraa <136384502+ezequiasMouraa@users.noreply.github.com>
2023-10-02 11:11:20 -0300

    Alguns ajustes no código

--- js/Typing.js
+++ js/Typing.js
@@ -5,19 +5,18 @@ function typeWriter(textElement, text, i) {
         i++;
         setTimeout(function () {
             typeWriter(textElement, text, i);
-        }, 50); // Ajuste o valor do intervalo para controlar a velocidade da digitação
+        }, 50); // Ajusta o valor do intervalo para controlar a velocidade da digitação
     }
 }
 
-// Inicie a animação de digitação quando o documento estiver pronto
 document.addEventListener("DOMContentLoaded", function () {
     var textElement = document.getElementById("typing-text");
     var text = textElement.textContent; // Obtém o texto original (sem HTML)
 
-    // Limpe o conteúdo original
+    // Limpa o conteúdo original
     textElement.innerHTML = "";
 
-    // Crie elementos para a digitação progressiva e a barra vertical
+    // Cria elementos para a digitação progressiva e a barra vertical
     var typingSpan = document.createElement("span");
     typingSpan.className = "typing";
     textElement.appendChild(typingSpan);
@@ -27,6 +26,6 @@ document.addEventListener("DOMContentLoaded", function () {
     blinkSpan.innerHTML = "|";
     textElement.appendChild(blinkSpan);
 
-    // Inicie a digitação progressiva
+    // Inicia a digitação progressiva
     typeWriter(typingSpan, text, 0);
 });
